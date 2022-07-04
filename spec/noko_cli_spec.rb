4a28bd400d7f14e7e47016091fa82a399988e86c
Juan Vásquez <juan@ombulabs.com>
2022-07-03 22:45:37 -0500

    chore: Change standard to rubocop (#27)
    - [x] Update RuboCop in PreCommit (overcommit).
    - [x] Fix rubocop analysis
    - [x] Install rubocop-rake, rubocop-rspec and rubocop-performance

--- spec/noko_cli_spec.rb
+++ spec/noko_cli_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe NokoCli do
   it "has a version number" do
-    expect(NokoCli::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(true).to eq(true)
+    expect(NokoCli::VERSION).not_to be_nil
   end
 end
