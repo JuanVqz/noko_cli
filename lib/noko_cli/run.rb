09c557326167eb3046a118044e9bac3e52eb2161
Juan Vasquez <juan@ombulabs.com>
2023-01-21 19:44:51 -0600

    Add Tag class

--- lib/noko_cli/run.rb
+++ lib/noko_cli/run.rb
@@ -2,14 +2,17 @@
 
 module NokoCli
   class Run # :nodoc:
-    attr_reader :config
-
     def initialize
       @config = Config.new
+      @tags = Tag.new(@config.conn).list
     end
 
     def self.call
-      Entry.new(config.conn).list
+      new.call
+    end
+
+    def call
+      Entry.new(@config.conn).list
     end
   end
 end
---
+++ spec/noko_cli/tag_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.describe NokoCli::Tag do
+  describe "#list" do
+    let(:stubs) do
+      stub_request("tags", response: stub_response(fixture: "tags/list"))
+    end
+    let(:config) { NokoCli::Config.new(adapter: :test, stubs: stubs) }
+    let(:tag) { described_class.new(config.conn) }
+
+    it "returns an array" do
+      expect(tag.list).to be_an_instance_of(Array)
+    end
+
+    it "returns tag's attributes" do
+      attributes = %w[id name]
+
+      expect(tag.list.first.keys).to match_array attributes
+    end
+  end
+end
