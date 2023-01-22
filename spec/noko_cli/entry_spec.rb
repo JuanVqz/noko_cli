3120e219d2bbeaedbeea8873ecb9ffa2245df814
Juan Vasquez <juan@ombulabs.com>
2023-01-21 19:22:22 -0600

    Rename Entry class

--- spec/noko_cli/entry_spec.rb
+++ spec/noko_cli/entry_spec.rb
@@ -9,24 +9,24 @@ RSpec.describe NokoCli::Entry do
       stub_request("current_user/entries", response: stub_response(fixture: "entries/list"))
     end
     let(:config) { NokoCli::Config.new(adapter: :test, stubs: stubs) }
-    let(:entries) { described_class.new(config: config) }
+    let(:entry) { described_class.new(config.conn) }
 
     it "includes headers" do
       headers = /date |minutes |project |description/
 
-      expect { entries.list }.to output(headers).to_stdout
+      expect { entry.list }.to output(headers).to_stdout
     end
 
     it "includes first entry info" do
       entry_info = /2022-07-03 |60 |noko_cli |#development implementing list functionality, installing faraday/
 
-      expect { entries.list }.to output(entry_info).to_stdout
+      expect { entry.list }.to output(entry_info).to_stdout
     end
 
     it "includes second entry info" do
       entry_info = /2022-07-05 |90 |noko_cli |Rendering the entry #development #testing/
 
-      expect { entries.list }.to output(entry_info).to_stdout
+      expect { entry.list }.to output(entry_info).to_stdout
     end
   end
 end
