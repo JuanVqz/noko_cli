2d9eaa18a2979aadfd18fdeeb4d7500264cc1d29
Juan Vasquez <juan@ombulabs.com>
2023-01-21 22:56:09 -0600

    Enable tests back

--- spec/noko_cli/tag_spec.rb
+++ spec/noko_cli/tag_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe NokoCli::Tag do
-  it "is a skipped example"
   describe "#list" do
     let(:stubs) do
       stub_request("tags", response: stub_response(fixture: "tags/list"))
