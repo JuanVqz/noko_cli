8c07da693fdfd61a1644412e21043b15b6a940e5
Juan Vasquez <juan@ombulabs.com>
2023-02-26 15:03:07 -0600

    Concat description

--- lib/noko_cli/form.rb
+++ lib/noko_cli/form.rb
@@ -4,23 +4,29 @@ require "tty-prompt"
 
 module NokoCli
   class Form # :nodoc:
-    # rubocop:disable Metrics/MethodLength
-    # rubocop:disable Metrics/AbcSize
     def call
+      result = show_form
+      concat_description(result)
+    end
+
+    # rubocop:disable Metrics/AbcSize
+    def show_form
       prompt = TTY::Prompt.new
       date = Time.new.strftime("%Y-%m-%d")
-      projects = Project.new.list
-      tags = Tag.new.list
 
       prompt.collect do
-        key(:date).ask("*Date (yyyy-mm-dd)", default: date, required: true)
+        key(:date).ask("*Date (#{date})", default: date, required: true)
         key(:minutes).ask("*Time (in minutes)", convert: :int, required: true)
-        key(:project_name).select("*Select a project", projects, filter: true, required: true)
+        key(:project_name).select("*Select a project", Project.new.list, filter: true, required: true)
         key(:description).ask("*Description", required: true)
-        key(:tags).multi_select("Select some tags?", tags, filter: true, cycle: true)
+        key(:tags).multi_select("Select some tags?", Tag.new.list, filter: true, cycle: true)
       end
     end
-    # rubocop:enable Metrics/MethodLength
     # rubocop:enable Metrics/AbcSize
+
+    def concat_description(info)
+      info[:description] = [info[:description]].concat(info[:tags]).join(" ")
+      info
+    end
   end
 end
